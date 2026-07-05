2723f7065a871c583940f2862d8591b95fefdd53
Jazir <thahir.jazir@puig.com>
2026-07-05 23:37:51 +0300

    return multiple values

---
+++ Advanced Functions/Return_multi_values.py
@@ -0,0 +1,9 @@
+def get_values():
+    return 10, 30, 40
+a, b, c = get_values()
+print(a, b, c)
+
+def get_values():
+    return [10, 30, 40], [4, 8], [14, 20, 5]
+a, b, c = get_values()
+print(a, b, c)
