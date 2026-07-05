be5bbf2939347329b3bff77b61b7f2ec963e118e
Jazir <thahir.jazir@puig.com>
2026-07-05 23:28:04 +0300

    keyword **arguments

--- Keywords _VAR_Arg.py
+++ Keywords _VAR_Arg.py
@@ -3,3 +3,9 @@ def show_details(**kwargs):
         print(f"{key}: {value}")
         
 show_details(name="Jazir", age=25, city="New York")
+
+def display(*args, **kwargs):
+    print("Args:", args)
+    print("Kwargs:", kwargs)
+
+display(10, 20, name="John", age=30)
