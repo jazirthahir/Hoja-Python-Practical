642c60acf6cba5355857bf08d0ebdbbd6ef7a414
Jazir <thahir.jazir@puig.com>
2026-07-09 18:30:06 +0300

    payment

---
+++ Advanced Functions/calculator.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    Total = a+b
+    return Total
+
+Answer = add(1, 10)
+print(Answer)
+
+def sub(a,b,c):
+    Total = a-b-c
+    return Total
+
+Answer = sub(100,5,1)
+print(Answer)
---
+++ Advanced Functions/payment.py
@@ -0,0 +1,12 @@
+def payment(total,gst,coupon): #total to sub coupon , total add gst
+    result = total + gst - coupon
+    return result
+
+total = 100
+gst = 18
+coupon = 40
+
+grand_total = payment(total,gst,coupon)
+print(grand_total)
+
+  
