684b4f9743770fc49183eaef95f205c321c2fc5d
Jazir <thahir.jazir@puig.com>
2026-07-16 18:41:15 +0300

    ques question practical

--- try_encapsulation.py
+++ try_encapsulation.py
@@ -1,17 +1,19 @@
-class animal:
+class Animal:
     def __init__(self):
         self.name = " I am spider"
         self._Speed = 50
         self.__weight = 45
         
     def details(self):
-        print("name :",self.name)
-        print("height :",self._Speed)
-        print("weight :",self.__weight)
+        print("name :", self.name)
+        print("height :", self._Speed)
+        
+    def get_weight(self):
+        return self.__weight
 
-spider = animal()
-cat = animal()
+spider = Animal()
+cat = Animal()
 
 print(self.name)
 print(self._speed)
-print(self.__speed)
+print(self.get_weight)
